98f8c6bd326a52e7626465a80c45f8da56b51a1b
Gianni <gianniicherry@gmail.com>
2023-06-14 14:24:36 -0700

    full working navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from "./components/Home";
+import Recipes from "./components/Recipes";
+import RecipeForm from './components/RecipeForm';
+import Navbar from './Navbar';
+import {Route, Routes} from "react-router-dom"
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+  <Navbar/>
+    <div className="container">
+      <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/recipes" element={<Recipes />}/>
+          <Route path="/recipeform" element={<RecipeForm />}/>
+      </Routes>
     </div>
-  );
+  </>
+  )
 }
 
 export default App;
---
+++ client/src/Navbar.js
@@ -0,0 +1,23 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+
+export default function Navbar(){
+    return ( <nav className="nav">
+    <Link to="/" className="site-title">Hangry.</Link>
+    <ul>
+        <CustomLink to="/recipes">Recipes</CustomLink>
+        <CustomLink to="/recipeform">Add Recipe</CustomLink>
+    </ul>
+</nav>
+    )
+}
+
+function CustomLink({ to, children, ...props}) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true})
+
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>{children}</Link>
+        </li>
+    )
+}
