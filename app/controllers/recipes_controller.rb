63aaa3f8f1198e9d56c984783fa4f4a8ddcd318d
Gianni <gianniicherry@gmail.com>
2023-06-14 17:23:41 -0700

    RESTfulresources for recipes and reviews

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,36 @@
 class RecipesController < ApplicationController
+
+    def index
+        recipes = Recipe.all 
+        render json: recipes 
+    end
+
+    def show
+        recipe = Recipe.find_by(id: params[:id])
+        render json: recipe
+    end
+
+    def create
+        recipe = Recipe.create(recipe_params)
+        render json: recipe, status: :created
+    end
+
+    def update 
+        recipe = Recipe.find_by(id: params[:id])
+        recipe.update(recipe_params)
+        render json: recipe
+    end
+
+    def destroy 
+        recipe = Recipe.find_by(id: params[:id])
+        recipe.destroy
+        head: no_content
+    end
+
+    
+    private 
+
+    def recipe_params
+        params.permit(:name, :cook_time, :ingredients, :instructions)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only:[:show,:create]
-  resources :reviews, only:[:show,:create,:update,:destroy]
-  resources :recipes, only:[:show,:index,:destroy,:create]
+  resources :reviews, only:[:index,:show,:create,:update,:destroy]
+  resources :recipes, only:[:index,:show,:destroy,:update,:create]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
